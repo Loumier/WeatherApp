6719711fb04ece64cc692d027830328537d8f4cb
Guilherme <loumier28@gmail.com>
2020-10-01 23:57:20 -0300

    Done

---
+++ WeatherApp/WeatherApp/Constants.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WeatherApp
+{
+    public static class Constants
+    {
+        public const string OpenWeatherMapEndpoint = "https://api.openweathermap.org/data/2.5/weather";
+        public const string OpenWeatherMapAPIKey = "02d49d7542ae7a0a730ed8f274343af7";
+    }
+}
